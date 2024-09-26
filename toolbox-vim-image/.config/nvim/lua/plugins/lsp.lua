a7d12226a8a51b988b9e02f2dd3c27577d700032
mattjmcnaughton <me@mattjmcnaughton.com>
2024-09-25 21:22:00 -0400

    wip: add treesitter, fugitive, and lsp
    Still locking them in.

---
+++ toolbox-vim-image/.config/nvim/lua/plugins/lsp.lua
@@ -0,0 +1,23 @@
+return {
+    "neovim/nvim-lspconfig",
+    dependencies = {
+        "williamboman/mason.nvim",
+        "williamboman/mason-lspconfig.nvim",
+        "hrsh7th/cmp-nvim-lsp",
+        "hrsh7th/cmp-buffer",
+        "hrsh7th/cmp-path",
+        "hrsh7th/cmp-cmdline",
+        "hrsh7th/nvim-cmp",
+        "L3MON4D3/LuaSnip",
+        "saadparwaiz1/cmp_luasnip",
+    },
+
+    config = function()
+        require("mason").setup()
+        require("mason-lspconfig").setup({
+            "gopls",
+            "rust_analyzer",
+            "lua_la"
+        })
+    end
+}
---
+++ toolbox-vim-image/.config/nvim/lua/plugins/treesitter.lua
@@ -0,0 +1,20 @@
+return {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    config = function()
+        require("nvim-treesitter.configs").setup({
+            ensure_installed = {
+                "rust", "go", "typescript", "bash", "lua", "haskell", "r",
+                "python", "json", "yaml", "nix", "dockerfile", "sql", "c",
+                "gitcommit"
+            },
+
+            sync_install = true,
+            auto_install = true,
+
+            indent = {
+                enable = true
+            },
+        })
+    end
+}
