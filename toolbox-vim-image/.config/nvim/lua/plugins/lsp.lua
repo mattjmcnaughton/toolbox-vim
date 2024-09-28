0cce67f07e0b6e9926129257e3817fb9eb61b5c6
mattjmcnaughton <me@mattjmcnaughton.com>
2024-09-27 20:57:44 -0400

    wip: conform.nvim and getting Mason working

--- toolbox-vim-image/.config/nvim/lua/plugins/lsp.lua
+++ toolbox-vim-image/.config/nvim/lua/plugins/lsp.lua
@@ -1,23 +1,34 @@
 return {
-    "neovim/nvim-lspconfig",
-    dependencies = {
-        "williamboman/mason.nvim",
-        "williamboman/mason-lspconfig.nvim",
-        "hrsh7th/cmp-nvim-lsp",
-        "hrsh7th/cmp-buffer",
-        "hrsh7th/cmp-path",
-        "hrsh7th/cmp-cmdline",
-        "hrsh7th/nvim-cmp",
-        "L3MON4D3/LuaSnip",
-        "saadparwaiz1/cmp_luasnip",
-    },
+	"neovim/nvim-lspconfig",
+	dependencies = {
+		"williamboman/mason.nvim",
+		"williamboman/mason-lspconfig.nvim",
+		"hrsh7th/cmp-nvim-lsp",
+		"hrsh7th/cmp-buffer",
+		"hrsh7th/cmp-path",
+		"hrsh7th/cmp-cmdline",
+		"hrsh7th/nvim-cmp",
+		"L3MON4D3/LuaSnip",
+		"saadparwaiz1/cmp_luasnip",
+		{"stevearc/conform.nvim", branch = "nvim-0.9"}
+		-- "mfussenegger/nvim-lint",
+	},
 
-    config = function()
-        require("mason").setup()
-        require("mason-lspconfig").setup({
-            "gopls",
-            "rust_analyzer",
-            "lua_la"
-        })
-    end
+	config = function()
+		require("mason").setup()
+		require("mason-lspconfig").setup({
+			ensure_installed = { "gopls" },
+		})
+
+		require("lspconfig").gopls.setup({})
+
+		require("conform").setup({
+			formatters_by_ft = {
+				lua = { "stylua" },
+			},
+			format_on_save = {
+				timeout_ms = 500,
+			}
+		})
+	end,
 }
